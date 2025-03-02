a302a16d73966916f62757533de2541b7b7e9637
Buthmika <buthmikamenushan@gmail.com>
2025-03-03 04:00:52 +0530

    Update staticClass.js

--- classes/staticClass.js
+++ classes/staticClass.js
@@ -1,9 +1,15 @@
 class square{
       constructor(width){
             this.width=width;
-            this.height=height;
+            this.height=width;
       }
       getArea(){
             return this.width*this.height;
       }
+      static calArea(a,b){
+            return a*b;
+      }
 }
+const newSquare =new square(10);
+console.log(newSquare.getArea());
+//console.log(square.calArea());
