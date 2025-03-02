f5bd114322910faa0594920ec5dab42033e7b77a
Buthmika <buthmikamenushan@gmail.com>
2025-03-03 05:10:36 +0530

    Update polymorphism.js

--- classes/polymorphism.js
+++ classes/polymorphism.js
@@ -7,8 +7,27 @@ class animal{
             console.log("Animal generally make sounds")
       }
 }
-class dog{
-
+class dog extends animal{
+      constructor(){
+            super(); 
+            console.log("Dog is created");
+      }
+      makeSound(){
+            console.log("Dogs normally barking")
+      }
+}
+class cat extends animal{
+      constructor(){
+            super();
+            console.log("Cat is created");
+      }
+      makeSound(){
+            console.log("Cats normally meaww");
+      }
 }
  const someAnimal=new animal;
+ const dog1=new dog;
+ const cat1=new cat;
  someAnimal.makeSound();
+ dog1.makeSound();
+ cat1.makeSound();
