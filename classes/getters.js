0181c868c0f655c393972e3af8b6a25c4b30946e
Buthmika <buthmikamenushan@gmail.com>
2025-03-03 03:46:59 +0530

    Update getters.js

--- classes/getters.js
+++ classes/getters.js
@@ -11,9 +11,12 @@ class square{
 
       }
       set area(newArea){
-
+           this.width=Math.sqrt(newArea);
+           this.height=Math.sqrt(newArea); 
       }
 }
 const newSquare=new square(10);
 console.log(newSquare.getArea());
+console.log(newSquare.area)
+newSquare.area=625;
 console.log(newSquare.area)
