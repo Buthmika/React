d1ba4135e9c99327cd6043dee296427dfc3809ae
Buthmika <buthmikamenushan@gmail.com>
2025-03-03 11:19:58 +0530

    Update callBack.js

---
+++ callBacks/callBack.js
@@ -0,0 +1,9 @@
+function openFile(){
+      console.log('File Opened');
+}
+function addSomethung(){
+      console.log('Something Added');
+}
+function removeSomething(){
+      console.log('Something Removed');
+}
