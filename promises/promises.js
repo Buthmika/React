a843a5e53dfb75481792c2b7ceaa8321daac9e02
Buthmika <buthmikamenushan@gmail.com>
2025-03-05 05:04:58 +0530

    Update promises.js

--- promises/promises.js
+++ promises/promises.js
@@ -1,8 +1,18 @@
 function takeShower(){
-      
+      return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                  console.log("You take shower");
+                  resolve("You take shower");
+            },3000);
+      })
 }
 function doHomeWork(){
-
+      return new Promise((resolve, reject) => {
+           setTimeout(()=>{
+                  console.log("You do your homework");
+                  resolve("You did it completed your homework");
+           },1000) ;
+      })
 }
 function play(){
 
