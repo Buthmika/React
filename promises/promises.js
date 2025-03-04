4fc7d18fcee2f39ca9feead4e88209c755dbcf6e
Buthmika <buthmikamenushan@gmail.com>
2025-03-05 04:50:39 +0530

    Update promises.js

--- promises/promises.js
+++ promises/promises.js
@@ -2,42 +2,43 @@
 
 function test1(callback){
       setTimeout(()=>{
+            console.log("This is function_1")
+            callback();
 
       },3000)
-      console.log("This is function_1")
-      callback();
+      
 }
 
 function test2(callback){
       setTimeout(()=>{
-
+            console.log("This is function_1")
+            callback();
       },3000)
-      console.log("This is function_1")
-      callback();
+      
 }
 
 function test3(callback){
       setTimeout(()=>{
-
+            console.log("This is function_3")
+            callback();
       },3000)
-      console.log("This is function_3")
-      callback();
+      
 }
 
 function test4(callback){
       setTimeout(()=>{
-
+            console.log("This is function_4")
+            callback();
       },3000)
-      console.log("This is function_4")
-      callback();
+      
 }
 
 function test5(callback){
       setTimeout(()=>{
-
+            console.log("This is function_5")
+            callback();
       },10000)
-      console.log("This is function_5")
-      callback();
+      
 }
 test1(()=>{
       test2(()=>{
