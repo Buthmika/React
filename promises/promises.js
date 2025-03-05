f7c4955b54d9a4e076b244f8ea317af29895b446
Buthmika <buthmikamenushan@gmail.com>
2025-03-05 05:32:14 +0530

    Update promises.js

--- promises/promises.js
+++ promises/promises.js
@@ -1,24 +1,45 @@
 function takeShower(){
       return new Promise((resolve,reject)=>{
             setTimeout(()=>{
-                  console.log("You take shower");
-                  resolve("You take shower");
+                  const shower=true;
+                  if(shower){
+                        console.log("You take shower");
+                        resolve("You take shower");
+                  }
+                  else{
+                        reject("You didnt shower")
+                  }
+                  
             },1000);
       })
 }
 function doHomeWork(){
       return new Promise((resolve, reject) => {
            setTimeout(()=>{
-                  console.log("You do your homework");
-                  resolve("You did it completed your homework");
+                  const work=true;
+                  if(work){
+                        console.log("You do your homework");
+                        resolve("You did it completed your homework");
+                  }
+                  else{
+                        reject("Didnt homework")
+                  }
+                  
            },3000) ;
       })
 }
 function play(){
       return new Promise((resolve, reject) => {
             setTimeout(()=>{
-                  console.log("What is play");
-                  resolve("You played what you like");
+                  const sport=true;
+                  if(sport){
+                        console.log("What is play");
+                        resolve("You played what you like");
+                  }
+                  else{
+                        reject("Didint Play");
+                  }
+                  
             })
       },3000)
 }
@@ -34,5 +55,8 @@ takeShower()
 .then((result)=>{
       console.log(result)
       console.log("All Done");
+})
+.catch((error)=>{
+      console.log(error);
 })
 
