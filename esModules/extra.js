f94ad595dd69f292717eb7a704d1bd39c261171b
Buthmika <buthmikamenushan@gmail.com>
2025-03-06 06:05:51 +0530

    update

---
+++ esModules/extra.js
@@ -0,0 +1,4 @@
+export const test="This is extra.js";
+function hello(name){
+      console.log(`Hello ${name}`);
+}
---
+++ esModules/index.js
@@ -0,0 +1,3 @@
+import(test,hello);
+console.log(test)
+console.log("test log from index.js")
