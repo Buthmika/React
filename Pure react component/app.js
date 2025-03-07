35e47c03285222fea3097f26f1316f2f6dc8f331
Buthmika <buthmikamenushan@gmail.com>
2025-03-07 06:58:10 +0530

    Update app.js

--- Pure react component/app.js
+++ Pure react component/app.js
@@ -13,7 +13,15 @@ const student=()=>{
 }
 const studentCard=()=>{
       return React.createElement(
-            
+           "div",
+           {},
+            React.createElement(
+                  "img",
+                  {src:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F7423887%2Freact_react_native_icon&psig=AOvVaw2OuOAsu5BFs1zbhDJfgrsy&ust=1741397062801000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCZ3Nzn9osDFQAAAAAdAAAAABAE",
+                   alt:"image" ,
+                   className:"image";    
+                  }
+            )
       )
 }
 const App=()=>{
