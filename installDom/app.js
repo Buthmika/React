c24178e2406307cc6458cb41cdc625dd7acfc3b3
Buthmika <buthmikamenushan@gmail.com>
2025-03-21 08:44:28 +0530

    Update app.js

--- installDom/app.js
+++ installDom/app.js
@@ -27,7 +27,17 @@ const studentCard=()=>{
 
       )
 }
-
+const App=()=>{
+      return React.createElement(
+            "div",
+            {},
+            // "This is a React App"
+            [React.createElement("h1",{},"My react app"),
+            React.createElement(studentCard),
+            React.createElement(studentCard)
+            ]
+      );
+};
 
 const container=document.getElementById('root');
 const root=ReactDOM.createRoot(container);
