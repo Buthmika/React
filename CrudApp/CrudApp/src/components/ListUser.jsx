f87ae25bea62f669832064ff01ac2aefd75f1877
Buthmika <buthmikamenushan@gmail.com>
2025-05-14 18:28:42 +0530

    Update ListUser.jsx

--- CrudApp/CrudApp/src/components/ListUser.jsx
+++ CrudApp/CrudApp/src/components/ListUser.jsx
@@ -5,12 +5,54 @@ function ListUser() {
     <div>
      <h1>List User</h1>
      <form action="">
-      <label htmlFor="">Name</label>
-      <input type="text" name="" id="" /><br />
+      <table>
+        <tbody>
+          <tr>
+            <th>
+            <label htmlFor="">Name</label>
+            
+            </th>
+            <td>
+            <input type="text" name="name" id="" /><br />
+            </td>
+          </tr>
+          <tr>
+            <th>
+            <label htmlFor="">Email</label>
+            
+            </th>
+            <td>
+            <input type="email" name="email" id="" /><br />
+              
+            </td>
+          </tr>
+          <tr>
+            <th>
+            <label htmlFor="">Mobile</label>
+            </th>
+            <td>
+            <input type="text" name="mobile" id="" /><br />
+            </td>
+          </tr>
+          <tr>
+            <th>
+
+            </th>
+            <td>
+            <button>Save</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      {/* <label htmlFor="">Name</label>
+      <input type="text" name="name" id="" /><br />
       <label htmlFor="">Email</label>
-      <input type="email" name="" id="" /><br />
+      <input type="email" name="email" id="" /><br />
       <label htmlFor="">Mobile</label>
-      <input type="text" name="" id="" />
+      <input type="text" name="mobile" id="" /><br />
+      <button>Save</button> */}
+
       
      </form>
     </div>
