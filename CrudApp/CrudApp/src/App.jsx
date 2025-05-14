356e26707541cda10f485f5fd3a434ab189865dc
Buthmika <buthmikamenushan@gmail.com>
2025-05-14 09:53:12 +0530

    Update App.jsx

--- CrudApp/CrudApp/src/App.jsx
+++ CrudApp/CrudApp/src/App.jsx
@@ -1,31 +1,26 @@
-import{BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  
-
   return (
     <>
       <h1>CRUD APP</h1>
-      <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">List User</Link>
-          </li>
-          <li>
-            <Link to="/create">Create User</Link>
-          </li>
-          <li>
-            <Link to="/edit">Edit User</Link>
-          </li>
-        </ul>
-      </nav>
-      </BrowserRouter>
-    </>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">List User</Link></li>
+            <li><Link to="/create">Create User</Link></li>
+            <li><Link to="/edit">Edit User</Link></li>
+          </ul>
+        </nav>
 
-    
-    
+        <Routes>
+          <Route path="/" element={<h2>List User</h2>} />
+          <Route path="/create" element={<h2>Create User</h2>} />
+          <Route path="/edit" element={<h2>Edit User</h2>} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
