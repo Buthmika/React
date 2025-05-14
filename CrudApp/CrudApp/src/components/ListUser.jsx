f8b8a07e5e5f820e0030eda67868c42319679675
Buthmika <buthmikamenushan@gmail.com>
2025-05-14 18:19:39 +0530

    Update ListUser.jsx

--- CrudApp/CrudApp/src/components/ListUser.jsx
+++ CrudApp/CrudApp/src/components/ListUser.jsx
@@ -6,9 +6,9 @@ function ListUser() {
      <h1>List User</h1>
      <form action="">
       <label htmlFor="">Name</label>
-      <input type="text" name="" id="" />
+      <input type="text" name="" id="" /><br />
       <label htmlFor="">Email</label>
-      <input type="email" name="" id="" />
+      <input type="email" name="" id="" /><br />
       <label htmlFor="">Mobile</label>
       <input type="text" name="" id="" />
       
