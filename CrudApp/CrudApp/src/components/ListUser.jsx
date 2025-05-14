1b7a306e6de63bab4e4b199767f52f83ea7d6b04
Buthmika <buthmikamenushan@gmail.com>
2025-05-14 18:18:07 +0530

    Update ListUser.jsx

--- CrudApp/CrudApp/src/components/ListUser.jsx
+++ CrudApp/CrudApp/src/components/ListUser.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function ListUser() {
   return (
     <div>
-     
+     <h1>List User</h1>
+     <form action="">
+      <label htmlFor="">Name</label>
+      <input type="text" name="" id="" />
+      <label htmlFor="">Email</label>
+      <input type="email" name="" id="" />
+      <label htmlFor="">Mobile</label>
+      <input type="text" name="" id="" />
+      
+     </form>
     </div>
   )
 }
